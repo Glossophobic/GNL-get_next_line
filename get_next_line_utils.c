898c9e59f901359c5fdadf85f62b1cf7410c0240
oubelhaj <90373269+Glossophobic@users.noreply.github.com>
2022-11-13 12:46:12 +0100

    ...

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -102,3 +102,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub[len] = 0;
 	return (sub);
 }
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*rtn;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if ((size_t)start > ft_strlen(s))
+		return (ft_strdup(""));
+	rtn = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	if (!rtn)
+		return (0);
+	while (i < len)
+	{
+		rtn[i] = *(s + start + i);
+		i++;
+	}
+	rtn[i] = '\0';
+	return (rtn);
+}
+
